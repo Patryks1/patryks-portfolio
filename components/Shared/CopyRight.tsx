da693f5d805c5f91dd8f247b2d7abcc22454dc48
PatrykS <slowinskipatryk1321@gmail.com>
2021-07-09 17:12:44 +0100

    Copy right

---
+++ components/Shared/CopyRight.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CopyRight = (): JSX.Element => {
+  return <h1 className="text-center text-white py-5">Patryk Slowinski © 2021</h1>;
+};
+
+export default CopyRight;
