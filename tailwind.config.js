2f17e8d21a767b9ef6d09ff17b254cc171e7adb9
patryk slowinski <slowinskipatryk1321@gmail.com>
2022-08-18 09:48:11 +0100

    Tech: Remove dead code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        landing:
-          "url('https://images.unsplash.com/photo-1625320014712-cc333e4e93a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')"
-      }),
       backgroundColor: {
         primary: '#222831',
         secondary: {
